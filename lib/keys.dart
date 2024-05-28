75f7882924143df074347925a5487f9903c7b10f
Usman-Click <47116342+Usman-Click@users.noreply.github.com>
2024-05-28 18:53:24 +0100

    updated keys

--- lib/keys.dart
+++ lib/keys.dart
@@ -1,6 +1,1 @@
-const int accountID = 21278504;
-const String authKey = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNjVjMWNlMWJjOTE2YTVmYzgyODEwYWM3NTllMjljZSIsInN1YiI6IjY2NGE2YzIxN2E0ZmJhOTBlZjc5NTg4YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._fI7hQFdREpxGeGcA5JqPNNc_b1a31R3C6PLoyEHRdY";   
-const String favBaseUrl = "https://api.themoviedb.org/3/account/$accountID/favorite";
-const  String imgBaseUrl = 'https://image.tmdb.org/t/p/w500';
-const String baseUrl ='https://api.themoviedb.org/3/trending/movie/day?language=en-US';
-const  String seriesUrl = "https://api.themoviedb.org/3/tv/popular";
+
